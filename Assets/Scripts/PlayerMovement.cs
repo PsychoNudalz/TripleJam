2c33631d802d0e6559c63021c94cea15228e0a22
Psycho_Nudalz <anson.aw0618@gmail.com>
2023-04-15 14:23:39 +0100

    Added movement rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,20 +13,38 @@ public class PlayerMovement : MonoBehaviour
     }
 
     [Header("Stats")]
+    [Header("Movement")]
     [SerializeField]
     private float moveSpeed = 2f;
 
+
     [SerializeField]
-    private float deadZone = .2f;
+    private float deadZone = .02f;
 
     [SerializeField]
     private float acceleration = .1f;
 
+    private float slowDownZone => (current_velocity.magnitude * current_velocity.magnitude) / (2 * acceleration);
+
+    [Header("Rotate")]
+    [SerializeField]
+    private float rotationSpeed = 2f;
+
+    private float rotateLerp = 0;
+
+    [Header("Target")]
     [SerializeField]
     private WaypointController waypoint;
 
     [SerializeField]
-    private Vector3 target;
+    private Vector3 target_pos;
+
+    [SerializeField]
+    private Vector3 target_rot;
+    
+    
+    [SerializeField]
+    private Vector3 target_forward;
 
     [Header("Current")]
     [SerializeField]
@@ -48,9 +66,9 @@ public class PlayerMovement : MonoBehaviour
         set => transform.position = value;
     }
 
-    private float distance => Vector3.Distance(target, position);
+    private float distance => Vector3.Distance(target_pos, position);
 
-    private Vector3 direction => (target - position).normalized;
+    private Vector3 direction => (target_pos - position).normalized;
     // Start is called before the first frame update
 
     private void Awake()
@@ -77,27 +95,13 @@ public class PlayerMovement : MonoBehaviour
         switch (moveStat)
         {
             case MoveStat.Still:
+                Move_Rotate(target_rot);
+
                 break;
             case MoveStat.Move:
-                if (distance < deadZone)
-                {
-                    current_velocity = Vector3.zero;
-                    position = target;
-                    ChangeState(MoveStat.Still);
-                }
-                else
-                {
-                    if (current_velocity.magnitude < moveSpeed)
-                    {
-                        current_velocity += direction * (acceleration * Time.deltaTime);
-                    }
-                    else if (current_velocity.magnitude > moveSpeed)
-                    {
-                        current_velocity = direction * moveSpeed;
-                    }
+                Move_Rotate(target_forward);
 
-                    cc.Move(current_velocity * Time.deltaTime);
-                }
+                Move_Translate();
 
                 break;
             case MoveStat.Freeze:
@@ -107,33 +111,107 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void Move_Translate()
+    {
+        if (distance < deadZone)
+        {
+            current_velocity = Vector3.zero;
+            position = target_pos;
+            ChangeState(MoveStat.Still);
+        }
+        else
+        {
+            if (distance < slowDownZone)
+            {
+                current_velocity -= direction * (acceleration * Time.deltaTime);
+            }
+            else
+            {
+                if (current_velocity.magnitude < moveSpeed)
+                {
+                    current_velocity += direction * (acceleration * Time.deltaTime);
+                }
+                else if (current_velocity.magnitude > moveSpeed)
+                {
+                    current_velocity = direction * moveSpeed;
+                }
+            }
+
+            transform.Translate(current_velocity * Time.deltaTime, Space.World);
+        }
+    }
+
+    void Move_Rotate(Vector3 target)
+    {
+        Vector3 transformEulerAngles = transform.eulerAngles;
+
+        if (rotateLerp > 1)
+        {
+            rotateLerp = 1;
+        }
+        else if (rotateLerp < 1)
+        {
+            rotateLerp += Time.deltaTime * rotationSpeed;
+        }
+        transformEulerAngles.y = Mathf.LerpAngle(transformEulerAngles.y,target.y,rotateLerp);
+
+
+        transform.eulerAngles = transformEulerAngles;
+    }
+   
+
     void ChangeState(MoveStat s)
     {
+        switch (moveStat)
+        {
+            case MoveStat.Still:
+                break;
+            case MoveStat.Move:
+                rotateLerp = 0;
+                break;
+            case MoveStat.Freeze:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
         moveStat = s;
+        switch (moveStat)
+        {
+            case MoveStat.Still:
+                break;
+            case MoveStat.Move:
+                rotateLerp = 0;
+                break;
+            case MoveStat.Freeze:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     public void Move_Forward()
     {
-        if (moveStat == MoveStat.Move)
+        if (moveStat != MoveStat.Still)
         {
             return;
         }
+        
         WaypointController newPoint = waypoint.GetConnected(transform.forward);
-        if (Vector3.Dot(waypoint.GetDir(newPoint), waypoint.forward) < 0)
+
+        target_forward = waypoint.GetDir(newPoint);
+
+        target_rot = newPoint.transform.eulerAngles;
+        if (Vector3.Dot(target_forward, newPoint.forward) < 0)
         {
-            transform.forward = -waypoint.forward;
+            target_rot.y -= 180;
         }
-        else
-        {
-            transform.forward = waypoint.forward;
-        }
+
+        target_forward = Quaternion.LookRotation(target_forward).eulerAngles;
 
         waypoint = newPoint;
-        
-        
-        target = waypoint.position;
-        ChangeState(MoveStat.Move);
 
-        
+
+        target_pos = waypoint.position;
+        ChangeState(MoveStat.Move);
     }
 }
