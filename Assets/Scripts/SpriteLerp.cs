48eab1a869afa554729d8122e6f07023b75b4cf5
Psycho_Nudalz <anson.aw0618@gmail.com>
2023-04-17 23:03:39 +0100

    Added code for shader morphing, added all doctor sprites

--- Assets/Scripts/SpriteLerp.cs
+++ Assets/Scripts/SpriteLerp.cs
@@ -1,27 +1,99 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpriteLerp : MonoBehaviour
 {
-    [Header("Textures")]
+    [Header("Material")]
     [SerializeField]
     Texture[] textures;
+
+    private Texture[] materialTextureQueue = new Texture[4];
     [SerializeField]
-    float lerpValue;
+    [Range(0f,1f)]
+    float lerpValue = 0f;
+
+    [Header("Settings")]
+    [SerializeField]
+    float lerpSpeed = 0.3f;
+    
+    [Header("Components")]
 
     [SerializeField]
     private Renderer renderer;
-    
-    // Start is called before the first frame update
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool increaseLerp = true;
+
+    private Material material;
+
+    private void Awake()
+    {
+        if (!renderer)
+        {
+            renderer = GetComponent<Renderer>();
+        }
+
+        material = renderer.material;
+    }
+
     void Start()
     {
-        
+        if (textures.Length <= 1)
+        {
+            enabled = false;
+            Debug.LogError($"{gameObject} missing textures");
+            return;
+        }
+
+        for (var i = 0; i < materialTextureQueue.Length; i++)
+        {
+            materialTextureQueue[i] = textures[0];
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (lerpValue > 1f)
+        {
+            ShiftMaterialTexture(GetRandomTexture());
+            lerpValue = 0;
+        }
+
+        if (increaseLerp)
+        {
+            lerpValue += Time.deltaTime * lerpSpeed;
+        }
+
+        material.SetFloat("_LerpValue",lerpValue);
+
+    }
+
+    Texture GetRandomTexture()
+    {
+        Texture randomTexture = textures[Random.Range(0, textures.Length)];
+        while (randomTexture.Equals(materialTextureQueue[0]))
+        {
+            randomTexture = textures[Random.Range(0, textures.Length)];
+        }
+        return randomTexture;
+    }
+
+    void ShiftMaterialTexture(Texture newTex)
+    {
+        materialTextureQueue[3] = materialTextureQueue[2];
+        materialTextureQueue[2] = materialTextureQueue[1];
+        materialTextureQueue[1] = materialTextureQueue[0];
+        materialTextureQueue[0] = newTex;
+        material.SetTexture("_Texture_1",materialTextureQueue[1]);
+        material.SetTexture("_Texture_2",materialTextureQueue[2]);
+        material.SetTexture("_Texture_3",materialTextureQueue[3]);
+        material.mainTexture = materialTextureQueue[0];
     }
 }
