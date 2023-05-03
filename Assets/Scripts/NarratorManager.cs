89d1dc4d07e7c47b401c45f0281dccb06007eebf
Psycho_Nudalz <anson.aw0618@gmail.com>
2023-05-03 23:33:23 +0100

    Refactor narrator to use queue

--- Assets/Scripts/NarratorManager.cs
+++ Assets/Scripts/NarratorManager.cs
@@ -14,13 +14,42 @@ public class NarratorManager : MonoBehaviour
 
         //Need to get interruption to work
         public bool canInterrupt;
-        public bool IsPlaying => sound.IsPlaying();
+        public bool IsActive => !scene.Equals(FlowScene.None);
+        public bool IsPlaying => IsActive && sound.IsPlaying();
+        public string Name => sound.GetClip().name;
 
-        public NarratorAudioSet(FlowScene scene, SoundAbstract sound, bool canInterrupt)
+
+        public NarratorAudioSet(FlowScene scene = FlowScene.None, SoundAbstract sound = null, bool canInterrupt = false)
         {
             this.scene = scene;
             this.sound = sound;
             this.canInterrupt = canInterrupt;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is NarratorAudioSet n)
+            {
+                if (n.scene.Equals(scene))
+                {
+                    return true;
+                }
+            }
+
+            if (obj is FlowScene o)
+            {
+                if (o.Equals(scene))
+                {
+                    return true;
+                }
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 
@@ -30,24 +59,24 @@ public class NarratorManager : MonoBehaviour
     [SerializeField]
     Queue<NarratorAudioSet> currentSetQueue = new Queue<NarratorAudioSet>();
 
-    private NarratorAudioSet currentSet => currentSetQueue.Peek();
+    private NarratorAudioSet currentSet;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        currentSet = new NarratorAudioSet();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (currentSetQueue.Count > 0)
+        if (currentSet.IsActive)
         {
             if (!currentSet.IsPlaying)
             {
-                currentSetQueue.Dequeue();
                 if (currentSetQueue.Count > 0)
                 {
-                    currentSet.sound.PlayF();
+                    PlayNextLine();
                 }
             }
         }
@@ -55,22 +84,51 @@ public class NarratorManager : MonoBehaviour
 
     public void PlayAudio(FlowScene flowScene)
     {
-        if (currentSet.IsPlaying)
+        CheckEndCurrentAudio();
+
+        QueueSets(flowScene);
+        PlayNextLine();
+    }
+
+    private void PlayNextLine()
+    {
+        if (currentSetQueue.Count == 0)
         {
-            currentSet.sound.Stop();
+            return;
         }
+        currentSet = currentSetQueue.Dequeue();
+        Debug.Log($"Narrator playing {currentSet.Name}");
 
         currentSet.sound.PlayF();
+    }
+
+    private bool CheckEndCurrentAudio()
+    {
+        if (currentSet.IsPlaying)
+        {
+            if (currentSet.canInterrupt)
+            {
+                currentSetQueue = new Queue<NarratorAudioSet>();
+                currentSet.sound.Stop();
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     void QueueSets(FlowScene flowScene)
     {
         foreach (NarratorAudioSet set in sets)
         {
-            if (set.scene.Equals(flowScene))
+            if (set.Equals(flowScene))
             {
                 currentSetQueue.Enqueue(set);
             }
         }
+
+        Debug.Log($"Narrator Queue size: {currentSetQueue.Count}");
     }
 }
